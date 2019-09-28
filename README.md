930eea50a9629ea9a42abf9249ad04186c4c776b
djs <david@lambdan.se>
2019-09-28 22:41:32 +0200

    update for New Twitch API

--- README.md
+++ README.md
@@ -8,11 +8,5 @@ No need to visit your Twitch following page anymore. Useful for [streamlink](htt
 
 - Install Colorama: `pip install colorama`
 - Download `whoislive.py`
-- Run it in a command prompt/terminal window
-
-# Re: Twitch Auth
-
-It says I get access to your email, which I guess is true, but I haven't seen my own email while developing this, so I really don't. I guess "email" is Twitch's fancy wording for the `user_read` scope which I need to see who you are following.
-(However the script runs client side so I don't see it anyway, and the `#access_token` is a hash so I don't see your oauth key in my web server logs either.)
-
-tl;dr: I won't know anything about you
+- Run it in a command prompt/terminal window: `python3 whoislive.py`
+	- I recommend setting up an alias in your prompt, so you can just type `wil` for example
--- whoislive.py
+++ whoislive.py
@@ -26,73 +26,128 @@ else:
     reload(sys).setdefaultencoding('utf-8')
 
 
-token_file = os.path.join(os.path.expanduser("~"), '.whoislive-token')
+token_file = os.path.join(os.path.expanduser("~"), '.whoislive2-token')
 clientid = "qr27075xc6n85gn944oj70qf0glly4"  # please dont abuse
-
-
-def streams(offset):
-    url = "https://api.twitch.tv/kraken/streams/followed?client_id=" + clientid + \
-        "&oauth_token=" + token + "&stream_type=live&offset=" + str(offset) + "&limit=25"
-    try:
-        response = urllib2.urlopen(url)
-        data = json.loads(response.read())
-    except urllib2.HTTPError as err:
-        if err.code == 401:
-            print("Error 401: Unauthorized. Deleting your cached token. Run me again to get a new token.")
-            os.remove(token_file)
-            sys.exit(1)
-        elif err.code == 404:
-            print("Error 404: Twitch is down?")
-            sys.exit(1)
-        else:
-            print("Error: " + err)
-            sys.exit(1)
-
-    for stream in data['streams']:
-        # print stream
-        name = stream['channel']['name']
-        game = stream['game']
-        status = stream['channel']['status']
-        url = stream['channel']['url']
-        viewers = str(stream['viewers'])
-        print(Fore.GREEN + name, Fore.YELLOW + status, Fore.RED + viewers + " viewers", end="\n")
-
-    totalstreams = data['_total']
-    return totalstreams
 
 
 # Get token and save it if there is none
 if not os.path.isfile(token_file):
-    print("Please visit this URL to get a token: ")
-    print("https://api.twitch.tv/kraken/oauth2/authorize?response_type=token&client_id=qr27075xc6n85gn944oj70qf0glly4&scope=user_read&redirect_uri=http://lambdan.se/d/whoislive.html")
-    print("")
-    input_token = raw_input('Paste token here: ')
+    input_username = raw_input('Enter your Twtich.tv username (or someone elses if you wanna pretend to be them): ')
 
-    print("Testing token...")
-    testURL = "https://api.twitch.tv/kraken?client_id=" + \
-        clientid + "&oauth_token=" + input_token.rstrip()
-    response = urllib2.urlopen(testURL)
+    print("Grabbing User ID...")
+    req = urllib2.Request('https://api.twitch.tv/helix/users?login=' + input_username)
+    req.add_header('Client-ID', clientid)
+    response = urllib2.urlopen(req)
     data = json.loads(response.read())
-    if data["token"]["valid"] is True:
-        print("Success! Hi " + data["token"]["user_name"])
-        print("Saving token...")
+    if len(data['data']) == 1:
+        print("Username: " + data['data'][0]['login'])
+        print("Saving user id to " + token_file + "...")
         f = open(token_file, 'w')
-        f.write("token:" + input_token.rstrip())
+        f.write("username:" + data['data'][0]['login'])
+        f.write("\n")
+        f.write("userid:" + data['data'][0]['id'])
         f.close()
     else:
-        print("FAIL! Try running me again and we'll try to re-authenticate")
+        print('FAILED!')
+        print('This could be because of:')
+        print('- User does not exist')
+        print('- Twitch changed their API')
+        print('- Or something weird just happened...')
+        print('Try again if you want to.')
         sys.exit(1)
 
-# We have a token, let's show who's live
+# We have a token file, read userid 
 if os.path.isfile(token_file):
     # Read token from file
     with open(token_file) as f:
         lines = f.readlines()
-    token = lines[0].split(":")[1].rstrip()
+    for l in lines:
+        if l.split(":")[0] == 'userid':
+            userid = l.split(":")[1].rstrip()
+            #print("DEBUG: user id: " + str(userid))
 
-    # get first 25 :) (yeah this is really weird structure wise but whatever)
-    totalstreams = streams(0)
-    offset = 25
-    while offset < totalstreams:
-        streams(offset)
-        offset += 25
+# We got the user id, now lets show who's live
+if userid:
+    # grab followed streams
+    req = urllib2.Request('https://api.twitch.tv/helix/users/follows?from_id=' + userid + '&first=100')
+    req.add_header('Client-ID', clientid)
+    response = urllib2.urlopen(req)
+    data = json.loads(response.read())
+    
+    # figure out total amount of channels
+    followed_channels = []
+    total = int(data['total'])
+    #print ('Followed channels:', total)
+
+    # iterate follows
+    for channel in data['data']:
+        #print (channel['to_name']) # to_id to_name
+        followed_channels.append(channel['to_id'])
+
+    if total > 100: # we need pagination
+        while len(followed_channels) < total:
+            pagination_cursor = data['pagination']['cursor']
+            #print ('DEBUG pagination cursor:', pagination_cursor)
+            req = urllib2.Request('https://api.twitch.tv/helix/users/follows?from_id=' + userid + '&first=100&after=' + pagination_cursor)
+            req.add_header('Client-ID', clientid)
+            response = urllib2.urlopen(req)
+            data = json.loads(response.read())
+            for channel in data['data']:
+                #print (channel['to_name']) # to_id to_name
+                followed_channels.append(channel['to_id'])
+
+    # validate follows to make sure there are no dupes because i dont trust my pagination skills
+    if len(followed_channels) != total:
+        print ("error! amount of parsed channels and followed channels reported from twitch do not match")
+        sys.exit(1)
+
+    for c in followed_channels:
+        if followed_channels.count(c) > 1:
+            print ("error! duplicate in followed channels:",c)
+            sys.exit(1)
+
+
+    #print ('DEBUG: len fc list:', len(followed_channels))
+
+    # generate urls that we will call to see who is live
+    # we need to do this because you can request max 100 channels per call
+    # this is a damn mess btw
+    urls_to_call = []
+    x = 0
+    while x < len(followed_channels):
+        i = 0
+        url = 'https://api.twitch.tv/helix/streams?'
+        while i < 100: # 100 per call
+            if i > 0:
+                url += "&" # the first user_id= uses the ? in /streams?
+            url += 'user_id=' + followed_channels[x]
+            i += 1
+            x += 1
+            if x == len(followed_channels): # we've gone through all channels
+                break
+        urls_to_call.append(url)
+            
+    #print ('debug x ', x) # should be the same as total and len(followed_channels)
+
+    # now call the urls and get who is actually live
+    live_channels = []
+    for url in urls_to_call:
+        req = urllib2.Request(url)
+        req.add_header('Client-ID', clientid)
+        response = urllib2.urlopen(req)
+        data = json.loads(response.read())
+        for channel in data['data']:
+            #print(channel['user_name'])
+            live_channels.append({'user': channel['user_name'], 'title': channel['title'], 'viewers': channel['viewer_count']})
+
+    #print ('Live channels:', len(live_channels))
+
+    # sort by most viewers 
+    live_channels_sorted_by_viewers = sorted(live_channels, key=lambda k: k['viewers'], reverse=True)
+
+    # print output if there are live channels
+    if len(live_channels) > 0:
+        for channel in live_channels_sorted_by_viewers: # user, title, viewers
+            print (Fore.GREEN + channel['user'], Fore.YELLOW + channel['title'], Fore.RED + str(channel['viewers']))
+    else:
+        print("No one you follow is live :(")
